4fdb62bb0b3c002e383768b34f4fa774b9658d57
PongsatornSoftthai <Pongsatorn.b@softthai.co.th>
2025-11-24 10:08:31 +0700

    create base backend

---
+++ backend/Models/TbBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend2.Models;
+
+public partial class TbBook
+{
+    public int NBookId { get; set; }
+
+    public string SBookName { get; set; } = null!;
+
+    public decimal NPrice { get; set; }
+
+    public int NQuantity { get; set; }
+
+    public DateTime? DRelease { get; set; }
+
+    public int NAuthorId { get; set; }
+
+    public DateTime? DUpdate { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DDeleted { get; set; }
+}
